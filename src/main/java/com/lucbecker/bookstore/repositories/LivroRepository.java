aa1a266c395c6b18dfad5bd1e6b9cec455c4afe3
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-09 09:46:51 -0300

    create interfaces repositories

---
+++ src/main/java/com/lucbecker/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.lucbecker.bookstore.repositories;
+
+import com.lucbecker.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
