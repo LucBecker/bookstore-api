50b2a3a4fd77ac2cf1568c165121e42e3b9e3efc
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-09 22:06:38 -0300

    resource delete categoria

--- src/main/java/com/lucbecker/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/lucbecker/bookstore/resources/CategoriaResource.java
@@ -46,4 +46,10 @@ public class CategoriaResource {
         Categoria newObj = service.update(id, objDTO);
         return ResponseEntity.ok().body(new CategoriaDTO(newObj));
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
