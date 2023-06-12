7c9a33ca9bcfe90dcaabc0a092fd6e14f4bb2335
Lucas Becker <lucas.andrade@riachuelo.com.br>
2023-06-12 15:03:45 -0300

    custom validation exceptions

--- src/main/java/com/lucbecker/bookstore/controller/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/lucbecker/bookstore/controller/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.lucbecker.bookstore.services.exceptions.DataIntegrityViolationExcepti
 import com.lucbecker.bookstore.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,4 +28,14 @@ public class ResourceExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<StandardError> validationErrors(MethodArgumentNotValidException e, ServletRequest request) {
+        ValidationError error = new ValidationError(System.currentTimeMillis(),
+                HttpStatus.BAD_REQUEST.value(), "Field validation error");
+
+        for(FieldError x : e.getBindingResult().getFieldErrors()){
+            error.addErrors(x.getField(), x.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 }
